3bd8d4ac1c6859da2072dc76aa6124b6680dbf86
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-09-13 16:49:43 -0300

    Update AlugarVeiculoUI.java
    Mod UI Aluguel

--- src/main/java/br/com/ui/AlugarVeiculoUI.java
+++ src/main/java/br/com/ui/AlugarVeiculoUI.java
@@ -1,5 +1,8 @@
 package br.com.ui;
 
+import br.com.enums.TipoPessoa;
+import br.com.enums.TipoVeiculo;
+import br.com.list.ListaAluguel;
 import br.com.list.ListaVeiculos;
 import br.com.models.Aluguel;
 import br.com.models.PessoaFisica;
@@ -35,7 +38,7 @@ public class AlugarVeiculoUI extends BasicUI{
                 "Informar Tipo do Veiculo",
                 "Informar Tipo Pessoa",
                 "Informar CPF/CNPJ",
-                "Salvar Alterações",
+                "Alugar Veiculo",
                 "Cancelar");
         switch (option) {
             case 0: {
@@ -46,6 +49,48 @@ public class AlugarVeiculoUI extends BasicUI{
                     ConsoleUI.mensagemTemporizada(ConsoleUI.formatText("Placa digitada é inválida ou não existe!", "vermelho"), 2);
                 }
                 break;
+            } case 1 : {
+                String tipoStr = ConsoleUI.input("Informe o tipo do veiculo: 1 > PEQUENO, 2 > MEDIO, 3 > SUV");
+                int tipoInt = Integer.parseInt(tipoStr);
+                if(tipoInt == 1) {
+                    aluguel.setTipoVeiculo(TipoVeiculo.PEQUENO);
+                } else if (tipoInt == 2) {
+                    aluguel.setTipoVeiculo(TipoVeiculo.MEDIO);
+                } else if (tipoInt == 3){
+                    aluguel.setTipoVeiculo(TipoVeiculo.SUV);
+                } else {
+                    System.out.println("Valor digitado inválido!");
+                }
+                break;
+            } case 2 : {
+                String tipoStr = ConsoleUI.input("Informe o tipo pessoa: 1 > FISICA, 2 > JURIDICA");
+                int tipoInt = Integer.parseInt(tipoStr);
+                if(tipoInt == 1) {
+                    aluguel.setTipoPessoa(TipoPessoa.FISICA);
+                } else if (tipoInt == 2) {
+                    aluguel.setTipoPessoa(TipoPessoa.JURIDICA);
+                } else {
+                    System.out.println("Valor digitado inválido!");
+                }
+                break;
+            } case 3 : {
+                if (aluguel.getTipoPessoa().equals(TipoPessoa.FISICA)) {
+                    String cpf = ConsoleUI.input("Informe o CPF do cliente");
+                    aluguel.setDocumentoPessoa(cpf);
+                } else if (aluguel.getTipoPessoa().equals(TipoPessoa.JURIDICA)) {
+                    String cnpj = ConsoleUI.input("Informe o CNPJ do cliente");
+                    aluguel.setDocumentoPessoa(cnpj);
+                }
+                break;
+            } case 4 : {
+                if (aluguel.getPlacaVeiculo() == null || aluguel.getTipoVeiculo() == null || aluguel.getTipoPessoa() == null || aluguel.getDocumentoPessoa() == null) {
+                    ConsoleUI.mensagemTemporizada(ConsoleUI.formatText("Ainda existem campos vazios", "amarelo"),2);
+                    break;
+                } else {
+                    ConsoleUI.mensagemTemporizada(ConsoleUI.formatText("Aluguel realizado com sucesso!", "verde"),3);
+                    ListaAluguel.adicionarItem(aluguel);
+                    return false;
+                }
             }
             default: {
                 return false;
