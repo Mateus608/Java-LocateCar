6f549c98b636af3d78a2bfc8490dc67e75a36e06
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-09-13 16:48:07 -0300

    Add UI Alguel

--- src/main/java/br/com/ui/AlugarVeiculoUI.java
+++ src/main/java/br/com/ui/AlugarVeiculoUI.java
@@ -1,9 +1,26 @@
 package br.com.ui;
 
 import br.com.list.ListaVeiculos;
+import br.com.models.Aluguel;
+import br.com.models.PessoaFisica;
+import br.com.models.PessoaJuridica;
+import br.com.models.Veiculo;
 import br.com.util.ConsoleUI;
 
 public class AlugarVeiculoUI extends BasicUI{
+
+    private final Aluguel aluguel;
+    private final Veiculo veiculo;
+    private final PessoaFisica pessoaFis;
+    private final PessoaJuridica pessoaJur;
+
+    public AlugarVeiculoUI(Aluguel aluguel, Veiculo veiculo, PessoaFisica pessoaFis, PessoaJuridica pessoaJur) {
+        this.aluguel = aluguel;
+        this.veiculo = veiculo;
+        this.pessoaFis = pessoaFis;
+        this.pessoaJur = pessoaJur;
+    }
+
 
     @Override
     public void superiorTela() {
@@ -22,7 +39,12 @@ public class AlugarVeiculoUI extends BasicUI{
                 "Cancelar");
         switch (option) {
             case 0: {
-
+                String placa = ConsoleUI.input("Informe a placa do veiculo");
+                if (!ListaVeiculos.verificarPlaca(placa) && veiculo.validaPlaca(placa)) {
+                    aluguel.setPlacaVeiculo(placa);
+                } else {
+                    ConsoleUI.mensagemTemporizada(ConsoleUI.formatText("Placa digitada é inválida ou não existe!", "vermelho"), 2);
+                }
                 break;
             }
             default: {
